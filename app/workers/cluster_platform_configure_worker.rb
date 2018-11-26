Persper/drepo-gitlab

Create namespces for projects in group cluster
Extend cluster_platform_configure_worker.rb to loop through each project
of the group and create the kubernetes namespace and restricted service
account for each project namespace.

And for projects of sub-groups too

app/workers/cluster_platform_configure_worker.rb
@@ -6,9 +6,33 @@ class ClusterPlatformConfigureWorker
 
   def perform(cluster_id)
     Clusters::Cluster.find_by_id(cluster_id).try do |cluster|
-      next unless cluster.cluster_project
+      if cluster.project_type?
+        configure_for_project(cluster)
+      elsif cluster.group_type?
+        configure_for_group(cluster, cluster.group)
+      end
+    end
 
-      kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace(cluster.cluster_project)
+  rescue ::Kubeclient::HttpError => err
+    Rails.logger.error "Failed to create/update Kubernetes namespace for cluster_id: #{cluster_id} with error: #{err.message}"
+  end
+
+  private
+
+  def configure_for_project(cluster)
+    return unless cluster.cluster_project
+
+    kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace_by_cluster_project(cluster.cluster_project)
+
+    Clusters::Gcp::Kubernetes::CreateOrUpdateNamespaceService.new(
+      cluster: cluster,
+      kubernetes_namespace: kubernetes_namespace
+    ).execute
+  end
+
+  def configure_for_group(cluster, group)
+    group.projects.each do |project|
+      kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace_by_project(project)
 
       Clusters::Gcp::Kubernetes::CreateOrUpdateNamespaceService.new(
         cluster: cluster,
@@ -16,7 +40,8 @@ class ClusterPlatformConfigureWorker
       ).execute
     end
 
-  rescue ::Kubeclient::HttpError => err
-    Rails.logger.error "Failed to create/update Kubernetes namespace for cluster_id: #{cluster_id} with error: #{err.message}"
+    group.children.each do |subgroup|
+      configure_for_group(cluster, subgroup)
+    end
   end
 end

chenyan19209219/gitlab-ce

app/workers/cluster_platform_configure_worker.rb
@@ -6,9 +6,33 @@ class ClusterPlatformConfigureWorker
 
   def perform(cluster_id)
     Clusters::Cluster.find_by_id(cluster_id).try do |cluster|
-      next unless cluster.cluster_project
+      if cluster.project_type?
+        configure_for_project(cluster)
+      elsif cluster.group_type?
+        configure_for_group_projects(cluster, cluster.group)
+      end
+    end
 
-      kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace(cluster.cluster_project)
+  rescue ::Kubeclient::HttpError => err
+    Rails.logger.error "Failed to create/update Kubernetes namespace for cluster_id: #{cluster_id} with error: #{err.message}"
+  end
+
+  private
+
+  def configure_for_project(cluster)
+    return unless cluster.cluster_project
+
+    kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace_by_cluster_project(cluster.cluster_project)
+
+    Clusters::Gcp::Kubernetes::CreateOrUpdateNamespaceService.new(
+      cluster: cluster,
+      kubernetes_namespace: kubernetes_namespace
+    ).execute
+  end
+
+  def configure_for_group_projects(cluster, group)
+    group.projects.each do |project|
+      kubernetes_namespace = cluster.find_or_initialize_kubernetes_namespace_by_project(project)
 
       Clusters::Gcp::Kubernetes::CreateOrUpdateNamespaceService.new(
         cluster: cluster,
@@ -16,7 +40,8 @@ class ClusterPlatformConfigureWorker
       ).execute
     end
 
-  rescue ::Kubeclient::HttpError => err
-    Rails.logger.error "Failed to create/update Kubernetes namespace for cluster_id: #{cluster_id} with error: #{err.message}"
+    group.children.each do |subgroup|
+      configure_for_group_projects(cluster, subgroup)
+    end
   end
 end