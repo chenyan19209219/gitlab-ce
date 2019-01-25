489ccc90814455782fef0dd1d69bedad5a9d534e
rpereira2 <rpereira@gitlab.com>
2019-01-25 19:51:21 +0530

    Add migration to add columns to error_tracking
    Add project_name and organization_name to hold the names of the selected
    project and its organization.
    Change the default of the enabled column to be false, so that on project
    import, the enabled column will default to false and will not cause the
    validations to fail since the token will not be present.
    Also allow api_url to be nil, since it can be nil if enabled is false.

--- app/models/error_tracking/project_error_tracking_setting.rb
+++ app/models/error_tracking/project_error_tracking_setting.rb
@@ -8,9 +8,11 @@ module ErrorTracking
 
     belongs_to :project
 
-    validates :api_url, length: { maximum: 255 }, public_url: true, url: { enforce_sanitization: true }
+    validates :api_url, length: { maximum: 255 }, public_url: true, url: { enforce_sanitization: true, ascii_only: true }, if: :enabled
 
-    validate :validate_api_url_path
+    validate :validate_api_url_path, if: :enabled
+
+    validates :token, presence: true, if: :enabled
 
     attr_encrypted :token,
       mode: :per_attribute_iv,
@@ -18,6 +20,31 @@ module ErrorTracking
       algorithm: 'aes-256-gcm'
 
     after_save :clear_reactive_cache!
+
+    def project_name
+      super || project_name_from_slug
+    end
+
+    def organization_name
+      super || organization_name_from_slug
+    end
+
+    def project_slug
+      slugs = get_slugs
+      slugs[1] if slugs.length >= 2
+    end
+
+    def organization_slug
+      slugs = get_slugs
+      slugs[0] if slugs.length >= 2
+    end
+
+    def self.build_api_url_from(api_host:, project_slug:, organization_slug:)
+      uri = Addressable::URI.parse("#{api_host}/api/0/projects/#{organization_slug}/#{project_slug}/")
+      uri.path = uri.path.squeeze('/')
+
+      uri.to_s
+    end
 
     def sentry_client
       Sentry::Client.new(api_url, token)
@@ -33,10 +60,18 @@ module ErrorTracking
       end
     end
 
+    def list_sentry_projects
+      with_reactive_cache('list_projects', {}) do |result|
+        { projects: result }
+      end
+    end
+
     def calculate_reactive_cache(request, opts)
       case request
       when 'list_issues'
         sentry_client.list_issues(**opts.symbolize_keys)
+      when 'list_projects'
+        sentry_client.list_projects
       end
     end
 
@@ -49,7 +84,31 @@ module ErrorTracking
 
     private
 
+    def project_name_from_slug
+      return nil if api_url.blank?
+
+      slugs = get_slugs
+      slugs[1].titleize if slugs.length >= 2
+    end
+
+    def organization_name_from_slug
+      return nil if api_url.blank?
+
+      slugs = get_slugs
+      slugs[0].titleize if slugs.length >= 2
+    end
+
+    def get_slugs
+      if api_url.present?
+        api_url.partition('/api/0/projects').last.split('/').reject(&:blank?)
+      else
+        []
+      end
+    end
+
     def validate_api_url_path
+      return if api_url.blank?
+
       unless URI(api_url).path.starts_with?('/api/0/projects')
         errors.add(:api_url, 'path needs to start with /api/0/projects')
       end
--- spec/models/error_tracking/project_error_tracking_setting_spec.rb
+++ spec/models/error_tracking/project_error_tracking_setting_spec.rb
@@ -15,11 +15,19 @@ describe ErrorTracking::ProjectErrorTrackingSetting do
 
   describe 'Validations' do
     context 'when api_url is over 255 chars' do
-      it 'fails validation' do
+      before do
         subject.api_url = 'https://' + 'a' * 250
+      end
 
+      it 'fails validation when enabled' do
         expect(subject).not_to be_valid
         expect(subject.errors.messages[:api_url]).to include('is too long (maximum is 255 characters)')
+      end
+
+      it 'passes validation when disabled' do
+        subject.enabled = false
+
+        expect(subject).to be_valid
       end
     end
 
@@ -28,6 +36,36 @@ describe ErrorTracking::ProjectErrorTrackingSetting do
         subject.api_url = "https://replaceme.com/'><script>alert(document.cookie)</script>"
 
         expect(subject).not_to be_valid
+      end
+    end
+
+    context 'when token missing' do
+      it 'fails validation when enabled' do
+        subject.token = nil
+
+        expect(subject).not_to be_valid
+      end
+
+      it 'passes validation when disabled' do
+        subject.token = nil
+        subject.enabled = false
+
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when api_url missing' do
+      it 'fails validation when enabled' do
+        subject.api_url = nil
+
+        expect(subject).not_to be_valid
+      end
+
+      it 'passes validation when disabled' do
+        subject.api_url = nil
+        subject.enabled = false
+
+        expect(subject).to be_valid
       end
     end
 
@@ -104,6 +142,56 @@ describe ErrorTracking::ProjectErrorTrackingSetting do
 
         expect(result).to be_nil
       end
+    end
+  end
+
+  describe '#project_slug' do
+    it 'returns slug when api_url is correct' do
+      subject.api_url = 'http://gitlab.com/api/0/projects/org-slug/project-slug'
+
+      expect(subject.project_slug).to eq('project-slug')
+    end
+
+    it 'returns nil when api_url is blank' do
+      subject.api_url = nil
+
+      expect(subject.project_slug).to be_nil
+    end
+  end
+
+  describe '#organization_slug' do
+    it 'returns slug when api_url is correct' do
+      subject.api_url = 'http://gitlab.com/api/0/projects/org-slug/project-slug'
+
+      expect(subject.organization_slug).to eq('org-slug')
+    end
+
+    it 'returns nil when api_url is blank' do
+      subject.api_url = nil
+
+      expect(subject.organization_slug).to be_nil
+    end
+  end
+
+  describe '.build_api_url_from' do
+    it 'correctly builds api_url with slugs' do
+      api_url = described_class.build_api_url_from(
+        api_host: 'http://sentry.com/',
+        organization_slug: 'org-slug',
+        project_slug: 'proj-slug'
+      )
+
+      expect(api_url).to eq('http://sentry.com/api/0/projects/org-slug/proj-slug/')
+    end
+
+    it 'correctly builds api_url without slugs' do
+      api_url = described_class.build_api_url_from(
+        api_host: 'http://sentry.com/',
+        organization_slug: nil,
+        project_slug: nil
+      )
+
+      expect(api_url).to eq('http://sentry.com/api/0/projects/')
     end
   end
 end
