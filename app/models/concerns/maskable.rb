b7395bce014fb4afbacfa1e09ec5f5ef6088db01
Matija Čupić <matteeyah@gmail.com>
2019-02-15 17:53:46 +0100

    Add Maskable concern for CI variables
    This adds a concern that abstracts the concept of masking a variable,
    including the RegEx for validation.

---
+++ app/models/concerns/maskable.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Maskable
+  extend ActiveSupport::Concern
+
+  # * Single line
+  # * No escape characters
+  # * No variables
+  # * No spaces
+  # * Minimal length of 8 characters
+  # * Absolutely no fun is allowed
+  REGEX = /^\w{8,}$/
+
+  def masked?
+    protected && REGEX.match?(value)
+  end
+end
---
+++ spec/models/concerns/maskable_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Maskable do
+  let(:variable) { build(:ci_variable) }
+
+  describe '#masked?' do
+    subject { variable.masked? }
+
+    context 'when the variable is not protected' do
+      before do
+        variable.protected = false
+      end
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when the variable does not match the regex' do
+      before do
+        variable.protected = true
+        variable.value = 'hello world'
+      end
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'when the variable is protected and matches the regex' do
+      before do
+        variable.protected = true
+        variable.value = 'helloworld'
+      end
+
+      it { is_expected.to eq(true) }
+    end
+  end
+
+  describe 'REGEX' do
+    subject { Maskable::REGEX }
+
+    it 'does not match strings shorter than 8 letters' do
+      expect(subject.match?('hello')).to eq(false)
+    end
+
+    it 'does not match strings with spaces' do
+      expect(subject.match?('hello world')).to eq(false)
+    end
+
+    it 'does not match strings with shell variables' do
+      expect(subject.match?('hello$VARIABLEworld')).to eq(false)
+    end
+
+    it 'does not match strings with escape characters' do
+      expect(subject.match?('hello\rworld')).to eq(false)
+    end
+
+    it 'does not match strings that span more than one line' do
+      string = <<~EOS
+        hello
+        world
+      EOS
+
+      expect(subject.match?(string)).to eq(false)
+    end
+
+    it 'matches valid strings' do
+      expect(subject.match?('helloworld')).to eq(true)
+    end
+  end
+end
