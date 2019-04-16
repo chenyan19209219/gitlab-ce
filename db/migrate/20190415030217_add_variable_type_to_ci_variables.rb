79d7ef7b8458bb9ee115af9db474164ba7fab673
Krasimir Angelov <kangelov@gitlab.com>
2019-04-17 09:51:51 +1200

    Split variable type migration into one per table
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/27112#note_161190046

--- db/migrate/20190415030217_add_variable_type_to_ci_variables.rb
+++ db/migrate/20190415030217_add_variable_type_to_ci_variables.rb
@@ -8,14 +8,10 @@ class AddVariableTypeToCiVariables < ActiveRecord::Migration[5.0]
   ENV_VAR_VARIABLE_TYPE = 1
 
   def up
-    %i(ci_group_variables ci_pipeline_schedule_variables ci_pipeline_variables ci_variables).each do |table_name|
-      add_column_with_default(table_name, :variable_type, :smallint, default: ENV_VAR_VARIABLE_TYPE)
-    end
+    add_column_with_default(:ci_variables, :variable_type, :smallint, default: ENV_VAR_VARIABLE_TYPE)
   end
 
   def down
-    %i(ci_group_variables ci_pipeline_schedule_variables ci_pipeline_variables ci_variables).each do |table_name|
-      remove_column(table_name, :variable_type)
-    end
+    remove_column(:ci_variables, :variable_type)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190415030217) do
+ActiveRecord::Schema.define(version: 20190416213631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
