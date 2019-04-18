2eae3534df77fc43d13758313d1626755ed7beb2
Natalia Tepluhina <tarya.se@gmail.com>
2019-04-18 10:53:06 +0300

    Added cases for requesting templates and type

--- spec/javascripts/ide/stores/modules/file_templates/mutations_spec.js
+++ spec/javascripts/ide/stores/modules/file_templates/mutations_spec.js
@@ -2,6 +2,9 @@ import state from '~/ide/stores/modules/file_templates/state';
 import mutations from '~/ide/stores/modules/file_templates/mutations';
 import * as types from '~/ide/stores/modules/file_templates/mutation_types';
 
+const mockFileTemplates = [['MIT'], ['CC']];
+const mockTemplateType = 'test';
+
 describe('IDE file templates mutations', () => {
   let mockedState;
 
@@ -28,6 +31,34 @@ describe('IDE file templates mutations', () => {
       mutations[types.RECEIVE_TEMPLATE_TYPES_ERROR](mockedState);
 
       expect(mockedState.isLoading).toBe(false);
+    });
+  });
+
+  describe(types.RECEIVE_TEMPLATE_TYPES_SUCCESS, () => {
+    it('sets templates loading to false', () => {
+      mutations[types.RECEIVE_TEMPLATE_TYPES_SUCCESS](mockedState, mockFileTemplates);
+
+      expect(mockedState.isLoading).toBe(false);
+    });
+
+    it('sets templates to payload', () => {
+      mutations[types.RECEIVE_TEMPLATE_TYPES_SUCCESS](mockedState, mockFileTemplates);
+
+      expect(mockedState.templates).toEqual(mockFileTemplates);
+    });
+  });
+
+  describe(types.SET_SELECTED_TEMPLATE_TYPE, () => {
+    it('sets templates type to selected type', () => {
+      mutations[types.SET_SELECTED_TEMPLATE_TYPE](mockedState, mockTemplateType);
+
+      expect(mockedState.selectedTemplateType).toBe(mockTemplateType);
+    });
+
+    it('sets templates to empty array', () => {
+      mutations[types.SET_SELECTED_TEMPLATE_TYPE](mockedState, mockTemplateType);
+
+      expect(mockedState.templates).toEqual([]);
     });
   });
 });
